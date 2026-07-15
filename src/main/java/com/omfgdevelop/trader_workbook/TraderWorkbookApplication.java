b071c80dbd3bad299da66dd83b4b682e1558c90e
aleksander.podshivalov <alek.podshivalov@x5.ru>
2026-07-15 23:05:56 +0500

    init

---
+++ src/main/java/com/omfgdevelop/trader_workbook/TraderWorkbookApplication.java
@@ -0,0 +1,13 @@
+package com.omfgdevelop.trader_workbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderWorkbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderWorkbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omfgdevelop/trader_workbook/TraderWorkbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omfgdevelop.trader_workbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraderWorkbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
